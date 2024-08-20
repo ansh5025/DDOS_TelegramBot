4f6c10f11649ec06f639bdade3911fbae60bf521
Ansh Rai <ansh.21scse1140002@galgotiasuniversity.edu.in>
2024-08-20 20:40:00 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,7 +8,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8')
+bot = telebot.TeleBot('7041808475:AAH6f9XfZJM_VKV2d2ceJMaCDDVAvCm3FG4')
 
 # Admin user IDs
 admin_id = ["1065510487"]
